68a993ad7edd9a3a824b8e830b252e593b46d249
Mamadou Sy <msbarane@email.com>
2026-06-25 15:55:48 -0400

    modif

--- app/notes.py
+++ app/notes.py
@@ -79,4 +79,4 @@ def est_reussi(moyenne: float) -> bool:
         True si la moyenne est supérieure ou égale à 60, sinon False.
     """
     valider_note(moyenne)
-    return moyenne >= 60
+    return moyenne >= 9
